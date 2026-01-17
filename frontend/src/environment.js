990a0b710977115354daaf0224066358339182bf
Bhavya Goyal <bhavyagoyal30122005@gmail.com>
2026-01-18 00:26:43 +0530

    Initial Commit

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,3 +1,8 @@
-const server = "http://localhost:8000"
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://friend-s-call-backend.onrender.com" :
+
+    "http://localhost:8000"
+
 
 export default server;
